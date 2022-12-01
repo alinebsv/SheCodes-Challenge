622885d1faef19a59fe35eba5e7319ce0a55e2fd
alinebsv <115201122+alinebsv@users.noreply.github.com>
2022-12-01 14:41:30 -0800

    Added main icon from API

--- src/script.js
+++ src/script.js
@@ -80,6 +80,13 @@ function showWeather(response) {
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  let currentTempIcon = document.querySelector("#current-temp-icon");
+  currentTempIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentTempIcon.setAttribute("alt", response.data.weather[0].main);
 }
 
 function searchCity(city) {
